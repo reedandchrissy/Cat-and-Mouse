fda4fc70a4560f5f7385bbc159d55a1b1337cb2c
castroja <castroveritas@gmail.com>
2019-04-19 21:10:24 -0400

    enemies now disappear when they collide with the player

--- js/interactions.js
+++ js/interactions.js
@@ -1,8 +1,11 @@
 
 var playerEl = document.querySelector('#weapon');
+var scene = document.querySelector('a-scene');
+var health = document.querySelector('#player').querySelector('#health');
 
 //Delete enemy after colliding with the player
 playerEl.addEventListener('collide', function (e) {
-  e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+  scene.removeChild(e.detail.body.el);
+  let currHP = parseInt(health.getAttribute('value')) - 1;
+  health.setAttribute('value', currHP);
 });
-
