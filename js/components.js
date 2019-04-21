d3bc5aa2027c17902330f4a7238be8e18e5ccc87
castroja <castroveritas@gmail.com>
2019-04-21 15:30:26 -0400

    completed spawn system

--- js/components.js
+++ js/components.js
@@ -21,14 +21,14 @@
 
         this.el.addEventListener('die', () => {
 
-            let score = document.querySelector('#scene').querySelector('#player').querySelector('#score');
-            let scene = document.querySelector('#scene');
-            let oldScore = parseInt(score.getAttribute('value'));
-            let counter = document.querySelector('#scene').querySelector('#counter');
-            let oldCounter = parseInt(counter.getAttribute('value'));
+          let score = document.querySelector('#scene').querySelector('#player').querySelector('#score');
+          let scene = document.querySelector('#scene');
+          let oldScore = parseInt(score.getAttribute('value'));
+          let counter = document.querySelector('#scene').querySelector('#counter');
+          let oldCounter = parseInt(counter.getAttribute('value'));
 
-            counter.setAttribute('value', oldCounter - 1);
-            score.setAttribute('value', oldScore + 1);
+          counter.setAttribute('value', oldCounter - 1);
+          score.setAttribute('value', oldScore + 1);
             scene.object3D.remove(this.el.object3D) //removes object from aframe; leaves behind invisible bounding box
             scene.removeChild(this.el) //removes object from DOM; used to delete bounding box
           });
@@ -37,23 +37,53 @@
     });
 
      AFRAME.registerComponent('spawn', {
+
       init: function () {
         for(let i = 0; i < 10; i++){
           spawnNewEnemy(randomPosition());
         }
+
+      },
+
+      tick: function (time, timeDelta) {
+        let enemiesLeft = parseInt(document.querySelector('#scene').querySelector('#player').querySelector('#counter').getAttribute('value'));
+        if(enemiesLeft == 0){
+          updateWaveCounter();
+          let wave = parseInt(document.querySelector('#scene').querySelector('#player').querySelector('#wave').getAttribute('value'));
+          if(wave < 4){
+            spawnNewWave(wave);
+          } else {
+            console.log("You win!");
+            //Insert win event here
+          }
+        }
+
       }
     });
 
+     const updateWaveCounter = function(){
+        let wave = document.querySelector('#scene').querySelector('#player').querySelector('#wave');
+        let oldWaveCounter = parseInt(document.querySelector('#scene').querySelector('#player').querySelector('#wave').getAttribute('value'));
+        wave.setAttribute('value', oldWaveCounter + 1);
+     };
+
+     const spawnNewWave = function(wave){
+          let spawnLimit = 5 + wave * 5; 
+          for(let i = 0; i < spawnLimit; i++){
+            spawnNewEnemy(randomPosition());
+          }
+     };
+
      const spawnNewEnemy = function(position) { 
-        let newEnemy = document.querySelector('#scene').querySelector('#base').cloneNode(true);
-        let counter = document.querySelector('#scene').querySelector('#counter');
-        let oldCounter = parseInt(counter.getAttribute('value'));
-        counter.setAttribute('value', oldCounter + 1);
-        newEnemy.setAttribute('position', position);
-        document.body.querySelector('#scene').appendChild(newEnemy);
+      let newEnemy = document.querySelector('#scene').querySelector('#base').cloneNode(true);
+      let counter = document.querySelector('#scene').querySelector('#counter');
+      let oldCounter = parseInt(counter.getAttribute('value'));
+      counter.setAttribute('value', oldCounter + 1);
+      newEnemy.setAttribute('position', position);
+      document.body.querySelector('#scene').appendChild(newEnemy);
     };
 
-     const randomPosition = function() { 
+    const randomPosition = function() { 
             let x = Math.floor((Math.random() * 20) + 10); // greater than 10, less than 30
             let z = Math.floor((Math.random() * 20) + 10); // greater than 10, less than 30
 
